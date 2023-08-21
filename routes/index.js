de0caa5f1786deb1e72a351b7628872d0f978829
cayirmedine <htc.mdn.2010@gmail.com>
2023-08-21 12:24:03 +0300

    Routes added to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+const userController = require("../../controllers/userController")
+const breweriesController = require("../../controllers/breweriesController")
+const { body, validationResult } = require("express-validator")
+const { verifyToken } = require("../../middlewares/userMiddlewares")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post(
+ "/register",
+ body("password")
+  .matches(/\d/)
+  .withMessage("Password must contain a number")
+  .matches(/[A-Z]/)
+  .withMessage("Password must contain a capital")
+  .isLength({ min: 8 })
+  .withMessage("Password must be at least 8 character"),
+ (req, res, next) => {
+  const error = validationResult(req).formatWith(({ msg }) => msg)
 
-module.exports = router;
+  const hasError = !error.isEmpty()
+
+  if (hasError) {
+   res.status(422).json({ status: "error", data: error.array() })
+  } else {
+   next()
+  }
+ },
+ userController.register
+)
+
+router.post("/login", userController.login)
+
+router.get("/breweries", verifyToken, breweriesController.getBreweries)
+
+module.exports = router
