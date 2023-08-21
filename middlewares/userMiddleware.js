c3a855a53ba000c9bfbc1a289c09c0ee49033e67
cayirmedine <htc.mdn.2010@gmail.com>
2023-08-21 11:40:32 +0300

    Verify token operation added to user middlewares

---
+++ middlewares/userMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken")
+var dotenv = require("dotenv")
+dotenv.config()
+
+module.exports = {
+ verifyToken: async (req, res, next) => {
+  const token =
+   req.body.token ||
+   req.query.token ||
+   req.headers["x-access-token"] ||
+   req.headers["authorization"]?.replace("Bearer ", "")
+
+  if (!token) {
+   return res.status(403).send("A token is required for authentication")
+  }
+
+  try {
+   const decoded = jwt.verify(token, process.env.TOKEN_KEY)
+   req.user = decoded
+  } catch (err) {
+   return res.status(401).send("Invalid Token")
+  }
+  return next()
+ },
+}
